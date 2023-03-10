3f89ad0d7f4b2c06da44a77993584a11b43b57ec
Balugayake <balugayake1998@gmail.com>
2023-03-10 10:47:11 +0530

    first commit5

--- server/server.js
+++ server/server.js
@@ -3,10 +3,6 @@ import { connectDB } from "./config/database.js";
 
 connectDB();
 
-app.get("/", (req, res, next) => {
-  res.send("<h1>Balu gayake</h1>");
-});
-
 app.listen(4000, () => {
   console.log(`server running on Port ${process.env.PORT}`);
 });
