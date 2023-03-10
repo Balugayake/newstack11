9e03ebbc09020bc634ce1db695a545a0cbba70cf
Balugayake <balugayake1998@gmail.com>
2023-03-10 11:03:56 +0530

    first commit5

--- server/app.js
+++ server/app.js
@@ -2,14 +2,21 @@ import express from "express";
 import dotenv from "dotenv";
 const app = express();
 app.use(express.json());
+import path from "path";
 export default app;
 
-dotenv.config({
-  path: "server/config/config.env",
-});
-
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({ path: "server/config/config.env" });
+}
+console.log(process.env.PORT);
 //routers
 
 import adminRoutes from "./routes/admin.js";
 
 app.use("/v1/api", adminRoutes);
+
+app.use(express.static(path.resolve("./firsttask/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve("./firsttask/build/index.html"));
+});
